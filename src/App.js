3acfe1456974b74a2df519d8f01b2c969c8daf79
AmanBisht01 <bishtaman1111@gmail.com>
2021-11-25 21:32:25 +0530

    renovation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Second from "./components/Home/Second";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Second />
+    </div>
+  );
+}
+
+export default App;
