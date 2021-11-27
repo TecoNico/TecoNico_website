943ce03d2ffd4be99b39e4b78e3ada7dd7bedd41
AmanBisht01 <bishtaman1111@gmail.com>
2021-11-27 13:40:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import Home from "./components/Home/Home";
-<<<<<<< HEAD
 import OurProduct from "./components/OurProduct/OurProduct";
 import Why from "./components/Why we/Why";
 import OurProduct2 from "./components/OurProduct/OurProduct2";
-=======
-import Why from "./components/Why we/Why";
->>>>>>> 5eb7519da7350b770135b712d2eb10ba382d3880
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Why />
-<<<<<<< HEAD
       <OurProduct />
       <OurProduct2 />
-=======
->>>>>>> 5eb7519da7350b770135b712d2eb10ba382d3880
     </div>
   );
 }
