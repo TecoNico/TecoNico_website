2b734cab6ffdee37c5d30333ab41f3320286acd9
AmanBisht01 <bishtaman1111@gmail.com>
2021-12-02 21:58:05 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./components/Home/Home";
 import OurProduct from "./components/OurProduct/OurProduct";
 import Why from "./components/Why we/Why";
 import OurProduct2 from "./components/OurProduct/OurProduct2";
-
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
       <Why />
       <OurProduct />
       <OurProduct2 />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,120 @@
+import React from "react";
+import "./contact.css";
+import Footer from "../../Assets/footer.png";
+import Fb from "../../Assets/icons/fb.png";
+import yt from "../../Assets/icons/yt.png";
+import Insta from "../../Assets/icons/insta.png";
+import Linkdin from "../../Assets/icons/linkdin.png";
+import Tweeter from "../../Assets/icons/tweeter.png";
+const contact = () => {
+  return (
+    <div>
+      {/* <div className="contactform"> */}
+      <div className="contactform-outer">
+        <div className="contactform-inner">
+          <div className="container-inner-21">
+            <div className="contact-head">Contact Details</div>
+            <div className="address-info">
+              <h3>Address</h3>
+              <p>Lorem ipsum dolor sit annet, consectetur</p>
+            </div>
+            <div className="email-info">
+              <h3>Email Address</h3>
+              <p>xyz@gmail.com</p>
+            </div>
+            <div className="phone-info">
+              <h3>Phone No.</h3>
+              <p>+91-1234585213</p>
+            </div>
+            <div className="TecoNicoimg">
+              <img src={Footer} alt="TecoNico" />
+            </div>
+            <div className="SocialMedia">
+              <div>
+                <h4 align="center"> Our Social Media Handles</h4>
+              </div>
+              <div className="social-container">
+                <a
+                  href="https://www.youtube.com/channel/UCxwKmIoGtvGW6OLW07kiUGw/featured"
+                  className="youtube social"
+                >
+                  <img src={yt} alt="TecoNico" />
+                </a>
+                <a
+                  href="https://www.facebook.com/teconicopvtltd"
+                  className="facebook social"
+                >
+                  <img src={Fb} alt="TecoNico" />
+                </a>
+                <a
+                  href="https://twitter.com/Teconicopvtltd"
+                  className="twitter social"
+                >
+                  <img src={Tweeter} alt="TecoNico" />
+                </a>
+                <a
+                  href="https://www.instagram.com/teconicopvtltd/"
+                  className="instagram social"
+                >
+                  <img src={Insta} alt="TecoNico" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/teconicopvtltd/"
+                  className="linkedin social"
+                >
+                  <img src={Linkdin} alt="TecoNico" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="container-inner-22">
+            <div className="input-div">
+              <input
+                className="name"
+                id="first-name"
+                type="text"
+                placeholder="  First Name  "
+              />
+              <label for="first-name"></label>
+              <input
+                className="name"
+                id="last-name"
+                type="type"
+                placeholder="Last Name"
+              />
+              <label for="last-name"></label>
+            </div>
+            <div className="input-div">
+              <input
+                className="email"
+                id="email"
+                type="email"
+                placeholder="Email Address"
+              />
+              <label for="email"></label>
+            </div>
+            <div className="input-div">
+              <label for="message"></label>
+              <textarea
+                className="msg"
+                id="message"
+                name="message"
+                rows="8"
+                cols="100"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <div className="btn-div">
+              <button className="btn" type="button">
+                Send Message
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+};
+
+export default contact;
