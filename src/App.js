6659d5644c3d0ffa515095bcc3bfd64700f6cfa4
AmanBisht01 <bishtaman1111@gmail.com>
2021-12-24 22:22:01 +0530

    fix0.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Announcement from "./pages/announcement/index";
-import HealthCare from './pages/healthcare';
-import HomeAutomation from './pages/homeautomation/index';
-import S1Route from './pages/demopage/s1route';
-import S2Route from './pages/demopage/s2route';
-import S3Route from './pages/demopage/s3route';
-import S4Route from './pages/demopage/s4route';
-
+import HealthCare from "./pages/healthcare";
+import HomeAutomation from "./pages/homeautomation/index";
+import S1Route from "./pages/demopage/s1route";
+import S2Route from "./pages/demopage/s2route";
+import S3Route from "./pages/demopage/s3route";
+import S4Route from "./pages/demopage/s4route";
+import Team from "./components/Team/Team";
 
 function App() {
   return (
@@ -18,6 +17,7 @@ function App() {
         <Route exact path="/2" element={<S2Route />} />
         <Route exact path="/3" element={<S3Route />} />
         <Route exact path="/4" element={<S4Route />} />
+        <Route exact path="/team" element={<Team />} />
         <Route exact path="/healthcare" element={<HealthCare />} />
         <Route exact path="/homeautomation" element={<HomeAutomation />} />
       </Routes>
--- src/components/Navbar/Nav2.js
+++ src/components/Navbar/Nav2.js
@@ -3,10 +3,10 @@ import "./Nav.css";
 const Nav = () => {
   return (
     <header className="">
-      <nav class="navbar navbar-expand-lg navbar-dark  ">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-dark ">
+        <div className="container-fluid">
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasExample"
@@ -14,39 +14,63 @@ const Nav = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
           <div
-            class="offcanvas offcanvas-start"
+            className="offcanvas offcanvas-start"
             tabindex="-1"
             id="offcanvasExample"
             aria-labelledby="offcanvasExampleLabel bg-dark"
           >
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasExampleLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasExampleLabel">
                 Menu
               </h5>
               <button
                 type="button"
-                class="btn-close text-reset"
+                className="btn-close text-reset"
                 data-bs-dismiss="offcanvas"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="navbar-nav  offcanvas-body">
-              <a class="nav-link   " aria-current="page" href="/">
+            <div className="navbar-nav  offcanvas-body">
+              <a className="nav-link   " aria-current="page" href="/">
                 Home
               </a>
-              <a class="nav-link " href="/healthcare">
-                Products
-              </a>
-              <a class="nav-link  " href="/homeautomation">
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Products
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a class="dropdown-item" href="/healthcare">
+                      Healthcare
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/homeautomation">
+                      HomeAutomation
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <a className="nav-link  " href="/homeautomation">
                 Blogs
               </a>
-              <a class="nav-link  ">Our Team</a>
-              <a class="nav-link  ">Coming Products</a>
-              <a class="nav-link  ">Contact Us</a>
+              <a className="nav-link">Our Team</a>
+              <a className="nav-link">Coming Products</a>
+              <a className="nav-link">Contact Us</a>
             </div>
           </div>
         </div>
