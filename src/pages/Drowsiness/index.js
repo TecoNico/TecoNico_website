7d6bb108d32065f268ea5edaf3a1b8e74ec02ef1
Aman Bisht <bishtaman1111@gmail.com>
2022-08-15 23:48:01 +0530

    .

--- src/pages/Drowsiness/index.js
+++ src/pages/Drowsiness/index.js
@@ -11,7 +11,6 @@ const index = () => {
       <>
         <Nav2 />
         <Home />
-        
       </>
     </div>
   );
--- src/pages/announcement/index.js
+++ src/pages/announcement/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Ourproject } from "../../components/HomePage/Ourproject";
 import Introduce from "../../components/Introduce/Introduce";
-import Nav from "../../components/Navbar/Nav";
+import Nav from "../../components/Navbar/Nav2";
 import Ourvision from "../../components/OurVision/Ourvision";
 import Team from "../../components/Team/Team";
 
@@ -9,6 +9,14 @@ export default function Announcement() {
   return (
     <>
       <Nav />
+      <div className="drowsiness-home ">
+        <div className="home-bg">
+          <div className="drowsiness-text">
+            <p>Don't wait for future..</p>
+            <p> ..Let's change the Present </p>
+          </div>
+        </div>
+      </div>
       <Introduce />
       <Ourvision />
       <Ourproject />
