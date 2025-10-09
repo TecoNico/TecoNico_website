cf577d3d3abf966bf50c643b68f8e67da82db8c4
Sunny Yadav <sunnyyadav13091999@gmail.com>
2025-10-09 12:23:00 +0530

    feat-update

--- src/components/Contact-Us/contact.js
+++ src/components/Contact-Us/contact.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaMailBulk } from "react-icons/fa";
 import image from "../../Assets/image.png";
 import "./contact.css";
 
-const contact = () => {
+const Contact = () => {
+
+   const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    const name = form.current.name.value;
+    const email = form.current.email.value;
+    const message = form.current.message.value;
+
+    // Encode the message and subject for Gmail
+    const subject = encodeURIComponent(`Message from ${name}`);
+    const body = encodeURIComponent(
+      `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+    );
+
+    // Open Gmail compose with pre-filled content
+    window.open(
+      `https://mail.google.com/mail/?view=cm&fs=1&to=developer@teconico.in&su=${subject}&body=${body}`,
+      "_blank"
+    );
+  };
+
+
+
   return (
     <>
       {/* <img src={image} alt="Healthcare" className="contact_image" /> */}
@@ -19,28 +44,31 @@ const contact = () => {
               <FaMapMarkerAlt color="white" size={"40px"} />
               <div>
                 <h3 style={{ color: "Cyan" }}>Address</h3>
-                <p style={{ color: "white" }}> 452-adress sm with client</p>
+                <p style={{ color: "white" }}> 6, Ajmer Rd, Nirman Nagar, Brijlalpura, Jaipur, Rajasthan 302019</p>
               </div>
             </div>
             <div className="icons">
               <FaPhoneAlt color="white" size={"40px"} />
               <div>
                 <h3 style={{ color: "Cyan" }}>Phone</h3>
-                <p style={{ color: "white" }}> +9184675 6456 2</p>
+                <p style={{ color: "white" }}> +918085035032</p>
               </div>
             </div>
             <div className="icons">
               <FaMailBulk color="white" size={"40px"} />
               <div>
                 <h3 style={{ color: "Cyan" }}>Email</h3>
-                <p style={{ color: "white" }}> teconicopvtltd@gmail.com</p>
+                <p style={{ color: "white" }}> developer@teconico.in</p>
               </div>
             </div>
           </div>
           <div className="contact_info_right">
             <div className="contact_form">
               <h1>Send Message</h1>
-              <form id="contact-form" class="form-horizontal" role="form">
+              <p style={{ color: "white" }}>
+                Click below to send us an email directly:
+              </p>
+              <form  ref= {form} onSubmit = {sendEmail} id="contact-form" class="form-horizontal" role="form">
                 <div class="form-group">
                   <div class="col-sm-12">
                     <input
@@ -49,7 +77,8 @@ const contact = () => {
                       id="name"
                       placeholder="NAME"
                       name="name"
-                      //   value=""
+                      // value={formData.name}
+                      // onChange={handleChange}
                       required
                     />
                   </div>
@@ -71,7 +100,7 @@ const contact = () => {
 
                 <textarea
                   class="form-control"
-                  rows="2"
+                  rows="3"
                   cols="40"
                   placeholder="MESSAGE"
                   name="message"
@@ -86,6 +115,7 @@ const contact = () => {
                   style={{ backgroundColor: "cyan", color: "white" }}
                   //   background-color="cyan"
                 >
+
                   <div class="alt-send-button">
                     <i class="fa fa-paper-plane"></i>
                     <span class="send-text">SEND</span>
@@ -100,4 +130,4 @@ const contact = () => {
   );
 };
 
-export default contact;
+export default Contact;
